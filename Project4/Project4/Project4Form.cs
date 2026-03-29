e69822802187be6731b31a8c826b49fa5fd6a841
Skinnybonz-Git <jrstevieb@gmail.com>
2026-03-28 23:23:49 -0400

    Add files via upload

---
+++ Project4/Project4/Project4Form.cs
@@ -0,0 +1,25 @@
+namespace Project4
+{
+    public partial class Project4Web : Form
+    {
+        public Project4Web()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radSmall_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
