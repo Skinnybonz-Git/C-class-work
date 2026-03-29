422aacdc15a362653fc352d5a59fe43d2cfd75db
Skinnybonz-Git <jrstevieb@gmail.com>
2026-03-29 00:44:00 -0400

    Add files via upload

--- Project4/Project4/Project4Form.Designer.cs
+++ Project4/Project4/Project4Form.Designer.cs
@@ -28,41 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblOrdAmount = new Label();
+            lblOrderAmount = new Label();
             lblDiscount = new Label();
             lblTotal = new Label();
-            lblDrink = new Label();
+            lblDrinkType = new Label();
             grpSize = new GroupBox();
             radioLarge = new RadioButton();
-            radioMed = new RadioButton();
+            radioMedium = new RadioButton();
             radioSmall = new RadioButton();
             grpOrdTotal = new GroupBox();
             txtTotal = new TextBox();
             txtDiscount = new TextBox();
-            txtOrdAmount = new TextBox();
+            txtOrderAmount = new TextBox();
             lblQuantity = new Label();
-            butCalcOrd = new Button();
-            butNewCus = new Button();
-            butExit = new Button();
+            btnCalculateOrder = new Button();
+            btnNewCus = new Button();
+            btnExit = new Button();
             chkEmployee = new CheckBox();
-            listDrink = new ListBox();
-            txtCusName = new TextBox();
-            txtGreet = new TextBox();
+            lstDrinkType = new ListBox();
+            txtCustomerName = new TextBox();
+            txtMessage = new TextBox();
             label6 = new Label();
             txtQuantity = new TextBox();
-            lblCusname = new Label();
+            lblCustomername = new Label();
             grpSize.SuspendLayout();
             grpOrdTotal.SuspendLayout();
             SuspendLayout();
             // 
-            // lblOrdAmount
+            // lblOrderAmount
             // 
-            lblOrdAmount.AutoSize = true;
-            lblOrdAmount.Location = new Point(18, 46);
-            lblOrdAmount.Name = "lblOrdAmount";
-            lblOrdAmount.Size = new Size(132, 25);
-            lblOrdAmount.TabIndex = 0;
-            lblOrdAmount.Text = "Order Amount:";
+            lblOrderAmount.AutoSize = true;
+            lblOrderAmount.Location = new Point(18, 46);
+            lblOrderAmount.Name = "lblOrderAmount";
+            lblOrderAmount.Size = new Size(132, 25);
+            lblOrderAmount.TabIndex = 0;
+            lblOrderAmount.Text = "Order Amount:";
             // 
             // lblDiscount
             // 
@@ -72,7 +72,6 @@
             lblDiscount.Size = new Size(86, 25);
             lblDiscount.TabIndex = 1;
             lblDiscount.Text = "Discount:";
-            lblDiscount.Click += label2_Click;
             // 
             // lblTotal
             // 
@@ -83,19 +82,19 @@
             lblTotal.TabIndex = 2;
             lblTotal.Text = "Total:";
             // 
-            // lblDrink
+            // lblDrinkType
             // 
-            lblDrink.AutoSize = true;
-            lblDrink.Location = new Point(520, 61);
-            lblDrink.Name = "lblDrink";
-            lblDrink.Size = new Size(100, 25);
-            lblDrink.TabIndex = 1;
-            lblDrink.Text = "Drink Type:";
+            lblDrinkType.AutoSize = true;
+            lblDrinkType.Location = new Point(520, 61);
+            lblDrinkType.Name = "lblDrinkType";
+            lblDrinkType.Size = new Size(100, 25);
+            lblDrinkType.TabIndex = 1;
+            lblDrinkType.Text = "Drink Type:";
             // 
             // grpSize
             // 
             grpSize.Controls.Add(radioLarge);
-            grpSize.Controls.Add(radioMed);
+            grpSize.Controls.Add(radioMedium);
             grpSize.Controls.Add(radioSmall);
             grpSize.Location = new Point(29, 41);
             grpSize.Name = "grpSize";
@@ -115,16 +114,16 @@
             radioLarge.Text = "&Large";
             radioLarge.UseVisualStyleBackColor = true;
             // 
-            // radioMed
+            // radioMedium
             // 
-            radioMed.AutoSize = true;
-            radioMed.Location = new Point(18, 90);
-            radioMed.Name = "radioMed";
-            radioMed.Size = new Size(103, 29);
-            radioMed.TabIndex = 1;
-            radioMed.TabStop = true;
-            radioMed.Text = "&Medium";
-            radioMed.UseVisualStyleBackColor = true;
+            radioMedium.AutoSize = true;
+            radioMedium.Location = new Point(18, 90);
+            radioMedium.Name = "radioMedium";
+            radioMedium.Size = new Size(103, 29);
+            radioMedium.TabIndex = 1;
+            radioMedium.TabStop = true;
+            radioMedium.Text = "&Medium";
+            radioMedium.UseVisualStyleBackColor = true;
             // 
             // radioSmall
             // 
@@ -136,14 +135,13 @@
             radioSmall.TabStop = true;
             radioSmall.Text = "&Small";
             radioSmall.UseVisualStyleBackColor = true;
-            radioSmall.CheckedChanged += radSmall_CheckedChanged;
             // 
             // grpOrdTotal
             // 
             grpOrdTotal.Controls.Add(txtTotal);
             grpOrdTotal.Controls.Add(txtDiscount);
-            grpOrdTotal.Controls.Add(txtOrdAmount);
-            grpOrdTotal.Controls.Add(lblOrdAmount);
+            grpOrdTotal.Controls.Add(txtOrderAmount);
+            grpOrdTotal.Controls.Add(lblOrderAmount);
             grpOrdTotal.Controls.Add(lblDiscount);
             grpOrdTotal.Controls.Add(lblTotal);
             grpOrdTotal.Location = new Point(29, 377);
@@ -157,6 +155,7 @@
             // 
             txtTotal.Location = new Point(156, 161);
             txtTotal.Name = "txtTotal";
+            txtTotal.ReadOnly = true;
             txtTotal.Size = new Size(159, 31);
             txtTotal.TabIndex = 16;
             txtTotal.TabStop = false;
@@ -165,17 +164,19 @@
             // 
             txtDiscount.Location = new Point(156, 104);
             txtDiscount.Name = "txtDiscount";
+            txtDiscount.ReadOnly = true;
             txtDiscount.Size = new Size(157, 31);
             txtDiscount.TabIndex = 15;
             txtDiscount.TabStop = false;
             // 
-            // txtOrdAmount
+            // txtOrderAmount
             // 
-            txtOrdAmount.Location = new Point(156, 40);
-            txtOrdAmount.Name = "txtOrdAmount";
-            txtOrdAmount.Size = new Size(159, 31);
-            txtOrdAmount.TabIndex = 14;
-            txtOrdAmount.TabStop = false;
+            txtOrderAmount.Location = new Point(156, 40);
+            txtOrderAmount.Name = "txtOrderAmount";
+            txtOrderAmount.ReadOnly = true;
+            txtOrderAmount.Size = new Size(159, 31);
+            txtOrderAmount.TabIndex = 14;
+            txtOrderAmount.TabStop = false;
             // 
             // lblQuantity
             // 
@@ -186,32 +187,35 @@
             lblQuantity.TabIndex = 2;
             lblQuantity.Text = "&Quantity";
             // 
-            // butCalcOrd
+            // btnCalculateOrder
             // 
-            butCalcOrd.Location = new Point(545, 468);
-            butCalcOrd.Name = "butCalcOrd";
-            butCalcOrd.Size = new Size(179, 67);
-            butCalcOrd.TabIndex = 5;
-            butCalcOrd.Text = "Calculate Order &Amount";
-            butCalcOrd.UseVisualStyleBackColor = true;
+            btnCalculateOrder.Location = new Point(545, 468);
+            btnCalculateOrder.Name = "btnCalculateOrder";
+            btnCalculateOrder.Size = new Size(179, 67);
+            btnCalculateOrder.TabIndex = 5;
+            btnCalculateOrder.Text = "Calculate Order &Amount";
+            btnCalculateOrder.UseVisualStyleBackColor = true;
+            btnCalculateOrder.Click += btnCalculate_Click;
             // 
-            // butNewCus
+            // btnNewCus
             // 
-            butNewCus.Location = new Point(114, 658);
-            butNewCus.Name = "butNewCus";
-            butNewCus.Size = new Size(185, 65);
-            butNewCus.TabIndex = 6;
-            butNewCus.Text = "&NewCustomer";
-            butNewCus.UseVisualStyleBackColor = true;
+            btnNewCus.Location = new Point(114, 658);
+            btnNewCus.Name = "btnNewCus";
+            btnNewCus.Size = new Size(185, 65);
+            btnNewCus.TabIndex = 6;
+            btnNewCus.Text = "&NewCustomer";
+            btnNewCus.UseVisualStyleBackColor = true;
+            btnNewCus.Click += btnNewCustomer_Click;
             // 
-            // butExit
+            // btnExit
             // 
-            butExit.Location = new Point(536, 658);
-            butExit.Name = "butExit";
-            butExit.Size = new Size(165, 44);
-            butExit.TabIndex = 7;
-            butExit.Text = "E&xit";
-            butExit.UseVisualStyleBackColor = true;
+            btnExit.Location = new Point(536, 658);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(165, 44);
+            btnExit.TabIndex = 7;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
             // 
             // chkEmployee
             // 
@@ -223,32 +227,32 @@
             chkEmployee.Text = "Employee?";
             chkEmployee.UseVisualStyleBackColor = true;
             // 
-            // listDrink
+            // lstDrinkType
             // 
-            listDrink.FormattingEnabled = true;
-            listDrink.ItemHeight = 25;
-            listDrink.Items.AddRange(new object[] { "Brewed", "Cappucino", "Latte" });
-            listDrink.Location = new Point(520, 102);
-            listDrink.Name = "listDrink";
-            listDrink.Size = new Size(227, 154);
-            listDrink.TabIndex = 11;
+            lstDrinkType.FormattingEnabled = true;
+            lstDrinkType.ItemHeight = 25;
+            lstDrinkType.Items.AddRange(new object[] { "Brewed", "Cappucino", "Latte" });
+            lstDrinkType.Location = new Point(520, 102);
+            lstDrinkType.Name = "lstDrinkType";
+            lstDrinkType.Size = new Size(227, 154);
+            lstDrinkType.TabIndex = 11;
             // 
-            // txtCusName
+            // txtCustomerName
             // 
-            txtCusName.Location = new Point(517, 312);
-            txtCusName.Name = "txtCusName";
-            txtCusName.Size = new Size(230, 31);
-            txtCusName.TabIndex = 12;
-            txtCusName.TabStop = false;
+            txtCustomerName.Location = new Point(517, 312);
+            txtCustomerName.Name = "txtCustomerName";
+            txtCustomerName.Size = new Size(230, 31);
+            txtCustomerName.TabIndex = 12;
+            txtCustomerName.TabStop = false;
             // 
-            // txtGreet
+            // txtMessage
             // 
-            txtGreet.Location = new Point(280, 612);
-            txtGreet.Name = "txtGreet";
-            txtGreet.ReadOnly = true;
-            txtGreet.Size = new Size(230, 31);
-            txtGreet.TabIndex = 13;
-            txtGreet.TabStop = false;
+            txtMessage.Location = new Point(280, 612);
+            txtMessage.Name = "txtMessage";
+            txtMessage.ReadOnly = true;
+            txtMessage.Size = new Size(230, 31);
+            txtMessage.TabIndex = 13;
+            txtMessage.TabStop = false;
             // 
             // label6
             // 
@@ -268,36 +272,36 @@
             txtQuantity.TabIndex = 15;
             txtQuantity.TabStop = false;
             // 
-            // lblCusname
+            // lblCustomername
             // 
-            lblCusname.AutoSize = true;
-            lblCusname.Location = new Point(370, 318);
-            lblCusname.Name = "lblCusname";
-            lblCusname.Size = new Size(141, 25);
-            lblCusname.TabIndex = 3;
-            lblCusname.Text = "&Customer Name";
+            lblCustomername.AutoSize = true;
+            lblCustomername.Location = new Point(370, 318);
+            lblCustomername.Name = "lblCustomername";
+            lblCustomername.Size = new Size(141, 25);
+            lblCustomername.TabIndex = 3;
+            lblCustomername.Text = "&Customer Name";
             // 
             // Project4Web
             // 
-            AcceptButton = butCalcOrd;
+            AcceptButton = btnCalculateOrder;
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            CancelButton = butNewCus;
+            CancelButton = btnNewCus;
             ClientSize = new Size(780, 735);
-            Controls.Add(lblCusname);
+            Controls.Add(lblCustomername);
             Controls.Add(txtQuantity);
             Controls.Add(label6);
-            Controls.Add(txtGreet);
-            Controls.Add(txtCusName);
-            Controls.Add(listDrink);
+            Controls.Add(txtMessage);
+            Controls.Add(txtCustomerName);
+            Controls.Add(lstDrinkType);
             Controls.Add(chkEmployee);
-            Controls.Add(butExit);
-            Controls.Add(butNewCus);
-            Controls.Add(butCalcOrd);
+            Controls.Add(btnExit);
+            Controls.Add(btnNewCus);
+            Controls.Add(btnCalculateOrder);
             Controls.Add(lblQuantity);
             Controls.Add(grpOrdTotal);
             Controls.Add(grpSize);
-            Controls.Add(lblDrink);
+            Controls.Add(lblDrinkType);
             Name = "Project4Web";
             Text = "Stephen Burns";
             grpSize.ResumeLayout(false);
@@ -310,28 +314,28 @@
 
         #endregion
 
-        private Label lblOrdAmount;
+        private Label lblOrderAmount;
         private Label lblDiscount;
         private Label lblTotal;
-        private Label lblDrink;
+        private Label lblDrinkType;
         private GroupBox grpSize;
         private GroupBox grpOrdTotal;
         private Label lblQuantity;
         private RadioButton radioLarge;
-        private RadioButton radioMed;
+        private RadioButton radioMedium;
         private RadioButton radioSmall;
-        private Button butCalcOrd;
-        private Button butNewCus;
-        private Button butExit;
+        private Button btnCalculateOrder;
+        private Button btnNewCus;
+        private Button btnExit;
         private CheckBox chkEmployee;
         private TextBox txtTotal;
         private TextBox txtDiscount;
-        private TextBox txtOrdAmount;
-        private ListBox listDrink;
-        private TextBox txtCusName;
-        private TextBox txtGreet;
+        private TextBox txtOrderAmount;
+        private ListBox lstDrinkType;
+        private TextBox txtCustomerName;
+        private TextBox txtMessage;
         private Label label6;
         private TextBox txtQuantity;
-        private Label lblCusname;
+        private Label lblCustomername;
     }
 }
--- Project4/Project4/Project4Form.cs
+++ Project4/Project4/Project4Form.cs
@@ -6,20 +6,101 @@ namespace Project4
         {
             InitializeComponent();
         }
-
-        private void label2_Click(object sender, EventArgs e)
+        // Button click: Calculate Order Amount
+        private void btnCalculate_Click(object sender, EventArgs e)
         {
+            // Check Customer Name
+            if (string.IsNullOrWhiteSpace(txtCustomerName.Text))
+            {
+                MessageBox.Show("Must enter a Customer Name before placing an order.");
+                txtCustomerName.Focus();
+                return;
+            }
+
+            // Clear Message Box
+            txtMessage.Clear();
+
+            // Validate Quantity
+            if (!int.TryParse(txtQuantity.Text, out int quantity))
+            {
+                MessageBox.Show("Please enter a numeric quantity");
+                txtQuantity.Focus();
+                return;
+            }
+
+            // Check Drink Type selection
+            string drinkType;
+            if (lstDrinkType.SelectedItem != null)
+            {
+                drinkType = lstDrinkType.SelectedItem.ToString();
+            }
+            else
+            {
+                drinkType = "";
+            }
+
+            // Calculate Order Amount
+            double orderAmount = 0;
+            if (radioSmall.Checked) orderAmount = 2.0 * quantity;
+            else if (radioMedium.Checked) orderAmount = 3.0 * quantity;
+            else if (radioLarge.Checked) orderAmount = 4.0 * quantity;
+
+            // Calculate Discount
+            bool isEmployee = chkEmployee.Checked;
+            double discount = 0;
+            if (isEmployee && quantity > 2) discount = orderAmount * 0.10;
+            else if (isEmployee && quantity <= 2) discount = orderAmount * 0.08;
+            else if (!isEmployee && quantity > 4) discount = orderAmount * 0.05;
+
+            // Calculate Total
+            double total = orderAmount - discount;
+
+            // Display amounts
+            txtOrderAmount.Text = orderAmount.ToString("C");
+            txtDiscount.Text = discount.ToString("C");
+            txtTotal.Text = total.ToString("C");
+
+            // Display message based on quantity
+            switch (quantity)
+            {
+                case 0:
+                    txtMessage.Text = $"Sorry {txtCustomerName.Text}, nothing was ordered";
+                    break;
+                case 1:
+                    txtMessage.Text = $"{txtCustomerName.Text}, your {drinkType} has arrived!";
+                    break;
+                case 2:
+                    txtMessage.Text = $"{txtCustomerName.Text}, double {drinkType} is coming quickly!";
+                    break;
+                default:
+                    txtMessage.Text = $"Beware! {txtCustomerName.Text}, your {quantity} {drinkType}s have surrounded you!";
+                    break;
+            }
 
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        // Button click: New Customer
+        private void btnNewCustomer_Click(object sender, EventArgs e)
         {
+            txtCustomerName.Clear();
+            txtQuantity.Clear();
+            txtOrderAmount.Clear();
+            txtDiscount.Clear();
+            txtTotal.Clear();
+            txtMessage.Clear();
 
+            radioSmall.Checked = true;
+            lstDrinkType.ClearSelected();
+            chkEmployee.Checked = false;
+
+            txtQuantity.Focus();
         }
 
-        private void radSmall_CheckedChanged(object sender, EventArgs e)
+        // Button click: Exit
+        private void btnExit_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
+
     }
 }
